03ebaa39d4a0be94cf9064c2684eb59cda33d8a1
Anuj <invincibleanuj1718@gmail.com>
2024-09-14 15:21:08 +0530

    Home/Implement delete function

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { AiFillDelete } from "react-icons/ai";
 import { MdOutlineEdit } from "react-icons/md";
+import toast, { Toaster } from "react-hot-toast";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
@@ -14,24 +15,42 @@ const Home = () => {
     // console.log(data.blogs);
     setPosts(data.blogs);
   };
+  const deletePost = async (id) => {
+    const response = await fetch(`http://localhost:5000/delete-blog/${id}`, {
+      method: "DELETE",
+    });
+    if (response.status === 200) {
+      toast.success("Blog deleted successfully");
+    } else {
+      toast.error("Something went wrong");
+    }
+  };
   return (
-    <div className="mt-10">
-      {posts.map((post) => {
-        return (
-          <div
-            key={post._id}
-            className="w-[40vw] mx-auto p-3  rounded-md shadow-[0_4px_6px_rgba(255,255,255,0.5)]"
-          >
-            <div className="flex justify-end text-lg gap-3">
-              <AiFillDelete className="text-gray-400 hover:text-red-400 cursor-pointer hover:scale-110 transition-all" />
-              <MdOutlineEdit className="text-gray-400 hover:text-yellow-300 cursor-pointer hover:scale-110 transition-all" />
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="mt-10">
+        {posts.map((post) => {
+          return (
+            <div
+              key={post._id}
+              className="w-[40vw] mx-auto p-3  rounded-md shadow-[0_4px_6px_rgba(255,255,255,0.5)]"
+            >
+              <div className="flex justify-end text-lg gap-3">
+                <AiFillDelete
+                  className="text-gray-400 hover:text-red-400 cursor-pointer hover:scale-110 transition-all"
+                  onClick={() => deletePost(post._id)}
+                />
+                <MdOutlineEdit className="text-gray-400 hover:text-yellow-300 cursor-pointer hover:scale-110 transition-all" />
+              </div>
+              <h2 className="text-lg font-bold my-2">{post.title}</h2>
+              <h3 className=" text-gray-400 font-semibold">
+                {post.description}
+              </h3>
             </div>
-            <h2 className="text-lg font-bold my-2">{post.title}</h2>
-            <h3 className=" text-gray-400 font-semibold">{post.description}</h3>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
